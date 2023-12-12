12f2055fa9cc5f705df53333102c6401ee1282c1
takuchan <takuchanapp@gmail.com>
2023-12-12 09:13:46 +0900

    :+1: Configure UDP

--- milkypublisher/milkypublisher/milky.py
+++ milkypublisher/milkypublisher/milky.py
@@ -1,5 +1,53 @@
-def main():
-    print('Hi from milkypublisher.')
+import rclpy
+from rclpy.node import Node
+from socket import socket, AF_INET, SOCK_DGRAM
+
+import time
+
+
+
+class MilkyPublisher(Node):
+
+    def __init__(self) -> None:
+        super().__init__("Milkypublisher")
+        self.logger = self.get_logger()
+        HOST = '127.0.0.1'
+        PORT = 4001
+        self.s = socket(AF_INET,SOCK_DGRAM)
+        self.s.bind((HOST,PORT))
+    
+    def main(self):
+        while True:
+            try :
+                # ③Clientからのmessageの受付開始
+                print('Waiting message')
+                message, cli_addr = self.s.recvfrom(8192)
+                message = message.decode(encoding='utf-8')
+                print(f'Received message is [{message}]')
+
+                # Clientが受信待ちになるまで待つため
+                time.sleep(1)
+
+                # # ④Clientへ受信完了messageを送信
+                # print('Send response to Client')
+                # self.s.sendto('Success to receive message'.encode(encoding='utf-8'), cli_addr)
+                
+                # TODO: ROS2はライブラリ上ネットワークの制約を書けているかもしれない。要検討
+
+            except KeyboardInterrupt:
+                print ('\n . . .\n')
+                self.s.close()
+                break   
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    milkyplubsher = MilkyPublisher()
+    rclpy.spin(milkyplubsher)
+    milkyplubsher.destroy_node()
+    rclpy.shutdown()
+    
+    
 
 
 if __name__ == '__main__':
